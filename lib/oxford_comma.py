7571a345ccba9f626b26d8224ec2a77e5d72ad79
eustachekamala <eustachejared25@gmail.com>
2024-09-03 15:33:35 +0300

    I used the join function to separate the array of the items

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
+# import ipdb
+'''This function takes a list of items and returns a string with the Oxford comma.'''
 def oxford_comma(items):
-    return None
+    #! Handle single-element list
+    if len(items) == 1:
+        return items[0]
+    
+    #! Handle two-element list
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    
+    #! Handle lists with more than two elements
+    else:
+        return ', '.join(items[:-1]) + f", and {items[-1]}"
+
